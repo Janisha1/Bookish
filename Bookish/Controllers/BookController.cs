eccea18ab5e8e40078a77207a6cebbd1ee199b8c
Janisha Patel <Janisha.Patel@softwire.com>
2023-08-15 14:42:54 +0100

    Book List Page Added

--- Bookish/Controllers/BookController.cs
+++ Bookish/Controllers/BookController.cs
@@ -13,9 +13,32 @@ public class BookController : Controller
         _logger = logger;
     }
 
+    [HttpGet("booklist")]
     public IActionResult Index()
     {
-        return View();
+        var books = new List<BookViewModel>
+        {
+            new BookViewModel(
+                 "9780330508117"
+                ,"The Hitchhiker's Guide to the Galaxy"
+            ){
+                CoverPhotoUrl = "https://assets-eu-01.kc-usercontent.com/bcd02f72-b50c-0179-8b4b-5e44f5340bd4/2b788053-b036-430c-8503-10437ff03616/hitchhikers-guide-to-the-galaxy-original-cover.jpg?w=200&h=350&auto=format&bg=%23efefef&fit=crop"
+                ,Blurb = "It's an ordinary Thursday lunchtime for Arthur Dent until his house gets demolished. The Earth follows shortly afterwards to make way for a new hyperspace express route, and his best friend has just announced that he's an alien."
+                ,Genre = "comedy science fiction"
+                ,YearPublished = 1979
+            },
+        new BookViewModel(
+                "9780330508117"
+                ,"The Hitchhiker's Guide to the Galaxy - ed 2"
+            ){
+                CoverPhotoUrl = "https://assets-eu-01.kc-usercontent.com/bcd02f72-b50c-0179-8b4b-5e44f5340bd4/2b788053-b036-430c-8503-10437ff03616/hitchhikers-guide-to-the-galaxy-original-cover.jpg?w=200&h=350&auto=format&bg=%23efefef&fit=crop"
+                ,Blurb = "It's an ordinary Thursday lunchtime for Arthur Dent until his house gets demolished. The Earth follows shortly afterwards to make way for a new hyperspace express route, and his best friend has just announced that he's an alien."
+                ,Genre = "comedy science fiction"
+                ,YearPublished = 1979
+            }
+        };
+
+        return View("booklist", books);
     }
 
     [HttpGet("book")]
@@ -23,12 +46,16 @@ public class BookController : Controller
     {
         var book = new BookViewModel(
              "9780330508117"
-            ,"The Hitchhiker's Guide to the Galaxy"
-        ){
-             CoverPhotoUrl = "https://assets-eu-01.kc-usercontent.com/bcd02f72-b50c-0179-8b4b-5e44f5340bd4/2b788053-b036-430c-8503-10437ff03616/hitchhikers-guide-to-the-galaxy-original-cover.jpg?w=200&h=350&auto=format&bg=%23efefef&fit=crop"
-            ,Blurb = "It's an ordinary Thursday lunchtime for Arthur Dent until his house gets demolished. The Earth follows shortly afterwards to make way for a new hyperspace express route, and his best friend has just announced that he's an alien."
-            ,Genre = "comedy science fiction"
-            ,YearPublished = 1979
+            , "The Hitchhiker's Guide to the Galaxy"
+        )
+        {
+            CoverPhotoUrl = "https://assets-eu-01.kc-usercontent.com/bcd02f72-b50c-0179-8b4b-5e44f5340bd4/2b788053-b036-430c-8503-10437ff03616/hitchhikers-guide-to-the-galaxy-original-cover.jpg?w=200&h=350&auto=format&bg=%23efefef&fit=crop"
+            ,
+            Blurb = "It's an ordinary Thursday lunchtime for Arthur Dent until his house gets demolished. The Earth follows shortly afterwards to make way for a new hyperspace express route, and his best friend has just announced that he's an alien."
+            ,
+            Genre = "comedy science fiction"
+            ,
+            YearPublished = 1979
         };
         return View("Book", book);
     }
